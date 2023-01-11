7cbc3be8e7279b9718b9bcdeb9ef6f2b4708eaf3
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2023-01-11 19:47:54 -0300

    Levantamiento de mi app en react native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello Im francisco</Text>
+      <Text style={styles.subText}>This will be my Expense tracker app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222831',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize : '30px',
+    fontWeight : 'bold',
+    color: '#fff'
+  },
+  subText:{
+    color: 'white',
+    textDecorationLine: 'underline'
+  }
 });
